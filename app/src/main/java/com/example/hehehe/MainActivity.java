174281f485b51edf3622d9054d2d87f53f6179fc
OyamadoKun <oyamado_kun@hotmail.com>
2022-05-25 00:43:42 +0700

    Базовый Ui, есть ошибка, описание под меткой TODO

--- app/src/main/java/com/example/hehehe/MainActivity.java
+++ app/src/main/java/com/example/hehehe/MainActivity.java
@@ -6,19 +6,27 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText text;
+
     private DatabaseHelper mDBHelper;
     private SQLiteDatabase mDb;
 
-
-    public void insert_student(String name, String surname) {
-        String query = "INSERT INTO students (name, surname) VALUES (" + name + ", " + surname + ")";
-        System.out.println(query);
+    //TODO: Понять что не работает здесь. Основная проблема в том, что при вводных данных Fff FffF выдаётся ошибка android.database.sqlite.SQLiteException: no such column: Fff (code 1 SQLITE_ERROR): , while compiling: INSERT INTO Students (name, surname) VALUES (Fff, FffF)
+    public void insertStudent(String fName, String fSurname) {
+        String query = "INSERT INTO Students (name, surname) VALUES (" + fName + ", " + fSurname + ")";
         mDb.execSQL(query);
     }
 
@@ -40,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
             // Заполняем клиента
             client.put("name",  cursor.getString(1));
-            client.put("age",  cursor.getString(2));
+            client.put("surname",  cursor.getString(2));
 
             // Закидываем клиента в список клиентов
             clients.add(client);
@@ -50,27 +58,38 @@ public class MainActivity extends AppCompatActivity {
         }
         cursor.close();
         return clients;
+    }
 
+    private void updateList() {
+        SimpleAdapter adapter = new SimpleAdapter(this, returnStudents(), R.layout.adapter_item, new String[]{"name", "surname"}, new int[]{R.id.textView, R.id.textView2});
+        ListView listView = (ListView)findViewById(R.id.listView);
+        listView.setAdapter(adapter);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Тут с БД работа
+        setContentView(R.layout.activity_main);
         mDBHelper = new DatabaseHelper(this);
         try {
             mDBHelper.updateDataBase();
         } catch (IOException mIOException) {
             throw new Error("UnableToUpdateDatabase");
         }
-
         mDb = mDBHelper.getWritableDatabase();
-        // Конец работы
-        insert_student("22", "33");
 
-        //setContentView(R.layout.activity_main);
 
-        System.out.println(returnStudents());
+        text = (EditText)findViewById(R.id.editText);
+        button = (Button)findViewById(R.id.button);
+
+
+        button.setOnClickListener(v -> {
+            String[] str = text.getText().toString().split(" ");
+            if (str.length == 2) {
+                insertStudent(str[0], str[1]);
+                updateList();
+            }
+        });
+        updateList();
     }
 }
